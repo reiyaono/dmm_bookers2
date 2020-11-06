8029f80fad676cf776eb6083a63cbc0de515362b
EC2 Default User <ec2-user@ip-172-31-40-63.ap-northeast-1.compute.internal>
2020-11-06 11:34:52 +0000

    remove unnecessary comment

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,4 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
-  #   title：空でないように設定してください。
-  # body：空でない、かつ最大200文字までに設定してください。
 end
